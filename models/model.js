9b2bede88fc4129e5adbaa53c2e9afb7df946f0c
Raphael Costa <raphael.costa@m4u.com.br>
2020-10-02 13:58:06 -0300

    create mock models

---
+++ models/model.js
@@ -0,0 +1,18 @@
+let users = [
+  { id: "1", messageIds: ["1", "2", "3"], username: 'Pederneiras Silva' },
+  { id: "2", messageIds: ["4", "6"], username: 'Aderbaldo Matias' },
+  { id: "3", messageIds: ["5"], username: 'Bozolina Catulha' },
+]
+
+let messages = [
+  { id: "1", text: 'Lorem Ipsum da Silva Sauro', userId: "1"},
+  { id: "2", text: 'Once uppon a time Ablufs play with Natchos', userId: "1"},
+  { id: "3", text: 'Klux is a happy person that loves pickles', userId: "1"},
+  { id: "4", text: 'Blatchos blublu', userId: "2"},
+  { id: "5", text: 'Glatchos blabla', userId: "3"},
+  { id: "6", text: 'Kirizia iz guruzis goronga', userId: "2"},
+]
+
+module.exports = {
+  users, messages
+}
